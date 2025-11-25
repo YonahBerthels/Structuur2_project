bd4d2ad687a9d159f0453ed99eda3f3692fb8c03
Yonah Berthels <yonah.berthels@gmail.com>
2025-11-25 19:00:28 +0100

    Toevoegen tekenen bord

---
+++ lib/config.h
@@ -0,0 +1,64 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// In this file some global variables will be declared such as colors, standard gem measurements
+#include <M5Unified.h>
+
+#define X_OFFSET 9
+#define Y_OFFSET 15
+
+#define GEM_SIZE 17
+#define GEM_VALUE 20
+
+#define MIN_TILT 0.15
+#define SPEED 3
+#define MEM_SIZE 512
+
+#define COLOR_BLACK M5.Lcd.color565(0, 0, 0)
+
+#define COLOR_RED M5.Lcd.color565(255, 0, 0)
+#define COLOR_GREEN M5.Lcd.color565(0, 255, 0)
+#define COLOR_BLUE M5.Lcd.color565(0, 0, 255)
+#define COLOR_YELLOW M5.Lcd.color565(255, 255, 0)
+#define COLOR_ORANGE M5.Lcd.color565(255, 165, 0)
+const u_int16_t COLORS[] = { COLOR_RED, COLOR_GREEN, COLOR_BLUE, COLOR_YELLOW, COLOR_ORANGE };
+
+#define SELECTION_COLOR M5.Lcd.color565(255, 255, 255)
+#define SELECTION_WIDTH 2
+
+/* Definition of Gem struct 
+* type:         the color of the gem (RED, GREEN, BLUE, YELLOW, GREEN, ORANGE)
+* x_lt:         x-coordinate of the left top
+* y_lt:         y-coordinate of the left top
+* is_selected:  has the user selected this gem? 
+*/
+typedef struct gem {
+    u_int8_t type;
+    u_int8_t x_lt;
+    u_int8_t y_lt;
+    bool is_selected;
+} Gem;
+
+/* Definition of Game struct 
+* level: t      he current level
+* moves_left:   how many moves the user has left (is reset upon level start)
+* score:        points scored
+* board_width:  width of the board
+* board_height: height of the board
+* gems:         the current state of the board
+*/
+typedef struct game {
+    u_int8_t level;
+    u_int8_t moves_left;
+    u_int16_t score;
+    u_int8_t board_width;
+    u_int8_t board_height;
+    Gem *gems;
+} Game;
+
+void draw_board(void);
+void init_gems(void);
+void init_game(Game*);
+void display_gamestate(void);
+
+#endif /* CONFIG_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -3,75 +3,75 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "EEPROM.h"
-#define MIN_TILT 0.15
-#define SPEED 3
-#define MEM_SIZE 512
+#include "../lib/config.h"
 
-void move_position(float, float);
-void save_position();
-void load_position();
-
-uint16_t black_color = M5.Lcd.color565(0, 0, 0);
-uint16_t red_color = M5.Lcd.color565(255, 0, 0);
-uint8_t rect_x, rect_y = 0, rect_w = 50, rect_h = 20;
-float acc_x, acc_y, acc_z;
+Game game;
 
 void setup() {
   M5.begin();
   M5.Imu.init();
   Serial.begin(115200);
   Serial.flush();
-  EEPROM.begin(MEM_SIZE); // schrijven van 0 t.e.m. 511
-  M5.Lcd.fillScreen(black_color);
+  M5.Lcd.fillScreen(COLOR_BLACK);
+
+  init_game(&game);
+  init_gems();
+  game.gems[10].is_selected = true;
 }
 
-void loop() {
+void init_game(Game *g) {
+  g->level = 1;
+  g->moves_left = 25;
+  g->score = 0;
+  g->board_height = 10;
+  g->board_width = 7;
+  // TODO: free this array/reseed the board
+  g->gems = (Gem*) calloc((g->board_height * g->board_width), sizeof(Gem));
+}
+
+void draw_board(void) {
+  for (int i = 0; i < game.board_height * game.board_width; i++) {
+    Gem g = game.gems[i];
+    u_int16_t color = COLORS[g.type];
+    M5.Lcd.fillRect(g.x_lt, g.y_lt, GEM_SIZE, GEM_SIZE, color);
+    // TODO: scale this to a selection of more than 1 square
+    if (g.is_selected)
+      M5.Lcd.drawRect(
+        (g.x_lt - SELECTION_WIDTH), 
+        (g.y_lt - SELECTION_WIDTH), 
+        GEM_SIZE + 2 * SELECTION_WIDTH, 
+        GEM_SIZE + 2 * SELECTION_WIDTH,
+        SELECTION_COLOR
+      );
+  }
+}
+
+void init_gems(void) {
+  srand(time(NULL));
+  for (int i = 0; i < game.board_width; i++) {
+    for (int j = 0; j < game.board_height; j++) {
+      Gem g = {
+        .type = rand() % 5,
+        .x_lt = X_OFFSET + (i * GEM_SIZE),
+        .y_lt = Y_OFFSET + (j * GEM_SIZE),
+        .is_selected = false, 
+      };
+      game.gems[i * game.board_height + j] = g;
+    }
+  }
+}
+
+void display_gamestate(void) {
+  M5.Lcd.setCursor(X_OFFSET, M5.Lcd.height() - 35);
+  M5.Lcd.printf("Moves left: %d\n", game.moves_left);
+    M5.Lcd.setCursor(X_OFFSET, M5.Lcd.height() - 25);
+  M5.Lcd.printf("Score: %d\n", game.score);
+}
+
+void loop(void) {
   M5.update();
   delay(20);
   M5.Lcd.clear();
-  M5.Imu.getAccelData(&acc_x, &acc_y, &acc_z);
-  Serial.printf("acc X: %f, acc Y: %f\n", acc_x, acc_y);
-  Serial.flush();
-  move_position(acc_x, acc_y);
-  if (M5.BtnA.isPressed())
-    save_position();
-  if (M5.BtnB.isPressed())
-    load_position();
-  M5.Lcd.fillRect(rect_x, rect_y, rect_w, rect_h, red_color);
-}
-
-void save_position() {
-  int addr = 0;
-  EEPROM.writeByte(addr, rect_x);
-  addr++;
-  EEPROM.writeByte(addr, rect_y);
-  EEPROM.commit();
-}
-
-void load_position() {
-  int addr = 0;
-  rect_x = EEPROM.readByte(addr);
-  addr++;
-  rect_y = EEPROM.readByte(addr);
-}
-
-void move_position(float acc_x, float acc_y) {
-  if (acc_x > MIN_TILT) {
-    M5.Lcd.fillRect(rect_x, rect_w, rect_w, rect_h, black_color);
-    rect_x -= SPEED;
-  } else if (acc_x < -MIN_TILT) {
-    M5.Lcd.fillRect(rect_x, rect_y, rect_w, rect_h, black_color);
-    rect_x += SPEED;
-  }
-  if (acc_y > MIN_TILT) {
-    M5.Lcd.fillRect(rect_x, rect_y, rect_w, rect_h, black_color);
-    rect_y += SPEED;
-  } else if (acc_y < -MIN_TILT) {
-    M5.Lcd.fillRect(rect_x, rect_y, rect_w, rect_h, black_color);
-    rect_y -= SPEED;
-  }
-  if (rect_y >= M5.Lcd.height())
-    rect_y = 0;
-  if (rect_x >= M5.Lcd.width())
-    rect_x = 0;
+  draw_board();
+  display_gamestate();
 }
